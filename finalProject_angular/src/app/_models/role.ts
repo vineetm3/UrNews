ab945c4c155026fa8f50930f1ca253e9d6ca455f
Vineet Marri <71222268+vineetm3@users.noreply.github.com>
2022-12-14 12:22:56 -0500

    Delete finalProject_angular directory

--- finalProject_angular/src/app/_models/role.ts
+++
@@ -1,4 +0,0 @@
-export enum Role {
-  user = 'User',
-  admin = 'Admin'
-}
--- finalProject_angular/src/app/_models/user.ts
+++
@@ -1,9 +0,0 @@
-import { Role } from './role';
-
-export class User {
-  username: string;
-  role: Role;
-  token?: string;
-  caloriegoal: number;
-  minutegoal: number;
-}
